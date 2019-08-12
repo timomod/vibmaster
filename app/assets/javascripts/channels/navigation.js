ed78c04c7d39290e737e62b1bd7d7b5c940d9017
Timomod <timomodeen@yahoo.com>
2019-08-12 18:13:59 +0200

    rails new with devsie installed

---
+++ app/assets/javascripts/channels/navigation.js
@@ -0,0 +1,14 @@
+ console.log("JS file loaded!");
+
+ $(window).scroll(function(){
+  	var scroll = $(window).scrollTop();
+	  if (scroll > 100) {
+	    $(".nav-bg").css({"background-color": "rgba(0,0,0,0.98)", "transition": "background-color 1.2s" });
+
+	  }
+
+	  else{
+		  $(".nav-bg").css({"background-color": "transparent"}); 
+
+	  }
+  })
